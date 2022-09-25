d3617cff3d8865dd7b081713f21573199857dc60
NataliaAraujo2 <102322514+NataliaAraujo2@users.noreply.github.com>
2022-09-25 04:36:57 -0300

    "Sem alterações

--- index.js
+++ index.js
@@ -1,56 +1,53 @@
-const server = require("./server")
-const database = require("./database")
-
+const server = require("./server");
+const database = require("./database");
 
 //POST tasks
-server.post("/tasks", function(req,res){
-    database("tasks").
-        insert(req.body).
-        then(function(data) {
-            res.status(201)
-            res.json(data)
-        }).
-        catch(function(error){
-            res.status(500)
-            res.json(error)
-        })
-        
-})
+server.post("/tasks", function (req, res) {
+  database("tasks")
+    .insert(req.body)
+    .then(function (data) {
+      res.status(201);
+      res.json(data);
+    })
+    .catch(function (error) {
+      res.status(500);
+      res.json(error);
+    });
+});
 //GET tasks
-server.get("/tasks", function(req,res){
-    database("tasks").
-        then(function(data){
-            res.json(data)
-        }).
-        catch(function(error){
-            res.status(500)
-            res.json(error)
-        })
-})
+server.get("/tasks", function (req, res) {
+  database("tasks")
+    .then(function (data) {
+      res.json(data);
+    })
+    .catch(function (error) {
+      res.status(500);
+      res.json(error);
+    });
+});
 
 //GET tasks/:id
-server.get("/tasks", function(req,res){
-    database("tasks").
-    then(function(data){
-        res.json(data)
-    }).
-    catch(function(error){
-        res.status(500)
-        res.json(error)
+server.get("/tasks", function (req, res) {
+  database("tasks")
+    .then(function (data) {
+      res.json(data);
     })
-
-})
+    .catch(function (error) {
+      res.status(500);
+      res.json(error);
+    });
+});
 
 //PATCH tasks/:id
-server.patch("/tasks", function(req,res){
-    res.status(404)
-    res.send("NOT FOUND")
-})
+server.patch("/tasks", function (req, res) {
+  res.status(404);
+  res.send("NOT FOUND");
+});
 
 //DELETE tasks/:id
-server.delete("/tasks", function(req,res){
-    res.status(404)
-    res.send("NOT FOUND")
-}) 
+server.delete("/tasks", function (req, res) {
+  res.status(404);
+  res.send("NOT FOUND");
+});
 
-server.listen(3000)
+server.listen(3000);
