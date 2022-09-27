5c9b503fcef0610ea43e110bdb719e3c86d1fcdf
NataliaAraujo2 <102322514+NataliaAraujo2@users.noreply.github.com>
2022-09-27 00:00:37 -0300

    Inclui arquivo knexfile

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+ module.exports = {
+  client: 'mysql2',
+  connection: {
+      host : process.env.DATABASE_HOST,
+      user : process.env.DATABASE_USER,
+      password : process.env.DATABASE_PASS,
+      database : process.env.DATABASE_NAME
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
+
